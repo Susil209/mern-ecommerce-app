47b361e34ddeaf41d34e2a74d9391898c113cc90
Susil <Susil.Nayak@cognizant.com>
2023-07-01 11:04:08 +0530

    order correction

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-
-require('dotenv').config();
+require("dotenv").config();
 const cors = require("cors");
 
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
-const jwt = require('jsonwebtoken');
-const cookieParser = require('cookie-parser');
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
 const productsRouter = require("./routes/Products");
 const categoriesRouter = require("./routes/Categories");
@@ -25,14 +24,13 @@ const LocalStrategy = require("passport-local").Strategy;
 const crypto = require("crypto");
 
 const { isAuth, sanitizeUser, cookieExtractor } = require("./services/common");
-const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
 
-const path= require('path');
-
+const path = require("path");
+const { Order } = require("./model/Order");
 
 // console.log(process.env);
-
 
 //WebHook
 // --- its like a api declared publically and any server can access it
@@ -40,40 +38,49 @@ const path= require('path');
 // This is your Stripe CLI webhook secret for testing your endpoint locally.
 const endpointSecret = process.env.ENDPOINT_SECRET;
 
-app.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
-  const sig = request.headers['stripe-signature'];
+app.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  async (request, response) => {
+    const sig = request.headers["stripe-signature"];
 
-  let event;
+    let event;
 
-  try {
-    event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
-  } catch (err) {
-    response.status(400).send(`Webhook Error: ${err.message}`);
-    return;
+    try {
+      event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+    } catch (err) {
+      response.status(400).send(`Webhook Error: ${err.message}`);
+      return;
+    }
+
+    // Handle the event
+    switch (event.type) {
+      case "payment_intent.succeeded":
+        const paymentIntentSucceeded = event.data.object;
+        // Then define and call a function to handle the event payment_intent.succeeded
+        const order = await Order.findById(
+          paymentIntentSucceeded.metadata.orderId
+        );
+        order.paymentStatus = "received";
+        await order.save();
+
+        break;
+      // ... handle other event types
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+
+    // Return a 200 response to acknowledge receipt of the event
+    response.send();
   }
-
-  // Handle the event
-  switch (event.type) {
-    case 'payment_intent.succeeded':
-      const paymentIntentSucceeded = event.data.object;
-      console.log(paymentIntentSucceeded);
-      // Then define and call a function to handle the event payment_intent.succeeded
-      break;
-    // ... handle other event types
-    default:
-      console.log(`Unhandled event type ${event.type}`);
-  }
-
-  // Return a 200 response to acknowledge receipt of the event
-  response.send();
-});
+);
 
 //JWT options
-const opts = {}
+const opts = {};
 opts.jwtFromRequest = cookieExtractor;
 opts.secretOrKey = process.env.JWT_SECRET_KEY;
 
-app.use(express.static(path.resolve(__dirname,'build')));
+app.use(express.static(path.resolve(__dirname, "build")));
 app.use(cookieParser());
 
 app.use(
@@ -87,10 +94,6 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(passport.authenticate("session"));
 
-
-
-
-
 //middlewares
 app.use(
   cors({
@@ -101,16 +104,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/products", isAuth(), productsRouter.router);
-app.use("/categories",isAuth(), categoriesRouter.router);
-app.use("/brands",isAuth(), brandsRouter.router);
-app.use("/users",isAuth(), usersRouter.router);
+app.use("/categories", isAuth(), categoriesRouter.router);
+app.use("/brands", isAuth(), brandsRouter.router);
+app.use("/users", isAuth(), usersRouter.router);
 app.use("/auth", authRouter.router);
-app.use("/cart",isAuth(), cartRouter.router);
-app.use("/orders",isAuth(), orderRouter.router);
+app.use("/cart", isAuth(), cartRouter.router);
+app.use("/orders", isAuth(), orderRouter.router);
+// this line we add to make react router work in case of other routes doesnt match
+app.get("*", (req, res) => res.sendFile(path.resolve("build", "index.html")));
 
 //Passport LocalStrategy
-passport.use('local',
-  new LocalStrategy({usernameField:'email'},async function (email, password, done) {
+passport.use(
+  "local",
+  new LocalStrategy({ usernameField: "email" }, async function (
+    email,
+    password,
+    done
+  ) {
     try {
       const user = await User.findOne({ email: email }).exec();
       if (!user) {
@@ -124,10 +134,13 @@ passport.use('local',
         "sha256",
         async function (err, hashedPassword) {
           if (!crypto.timingSafeEqual(user.password, hashedPassword)) {
-            return done(null, false, { message: 'Invalid credentials.' })
+            return done(null, false, { message: "Invalid credentials." });
           } else {
-            const token = jwt.sign(sanitizeUser(user), process.env.JWT_SECRET_KEY);
-            return done(null,  { id: user.id, role: user.role, token });
+            const token = jwt.sign(
+              sanitizeUser(user),
+              process.env.JWT_SECRET_KEY
+            );
+            return done(null, { id: user.id, role: user.role, token });
           }
         }
       );
@@ -137,22 +150,25 @@ passport.use('local',
   })
 );
 
-passport.use('jwt', new JwtStrategy(opts, async function (jwt_payload, done) {
-  try {
-    console.log(jwt_payload);
-    const user = await User.findById(jwt_payload.id);
-    if (user) {
-      return done(null, sanitizeUser(user));
-    } else {
-      return done(null, false);
-      // or you could create a new account
+passport.use(
+  "jwt",
+  new JwtStrategy(opts, async function (jwt_payload, done) {
+    try {
+      console.log(jwt_payload);
+      const user = await User.findById(jwt_payload.id);
+      if (user) {
+        return done(null, sanitizeUser(user));
+      } else {
+        return done(null, false);
+        // or you could create a new account
+      }
+    } catch (error) {
+      if (err) {
+        return done(err, false);
+      }
     }
-  } catch (error) {
-    if (err) {
-      return done(err, false);
-    }
-  }
-}));
+  })
+);
 
 //serialize and deserialize
 passport.serializeUser(function (user, cb) {
@@ -170,12 +186,9 @@ passport.deserializeUser(function (user, cb) {
   });
 });
 
-
 //payments
 // This is your test secret API key.
 const stripe = require("stripe")(process.env.STRIPE_SERVER_KEY);
-
-
 
 // const calculateOrderAmount = (items) => {
 //   // Replace this constant with a calculation of the order's amount
@@ -185,14 +198,17 @@ const stripe = require("stripe")(process.env.STRIPE_SERVER_KEY);
 // };
 
 app.post("/create-payment-intent", async (req, res) => {
-  const { totalAmount } = req.body;
+  const { totalAmount, orderId } = req.body;
 
   // Create a PaymentIntent with the order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: totalAmount*100,
+    amount: totalAmount * 100,
     currency: "inr",
     automatic_payment_methods: {
       enabled: true,
+    },
+    metadata: {
+      orderId,
     },
   });
 
@@ -201,10 +217,6 @@ app.post("/create-payment-intent", async (req, res) => {
   });
 });
 
-
-
-
-
 //databse connection
 main().catch((err) => console.log(err));
 
@@ -212,7 +224,6 @@ async function main() {
   await mongoose.connect(process.env.MONGODB_URL);
   console.log("Database connected");
 }
-
 
 app.listen(process.env.PORT, () => {
   console.log("Server is running on port 4000.");
